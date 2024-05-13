6bf539d3ebf9a6ffb69d87efce7101b26b5f7521
Vixyv <findlaymc@gmail.com>
2024-05-12 19:17:47 -0600

    Added Data Parser
    Moved all data analysis to "data_parser".
    The code is a bit sloppy right now because I've been pretty busy this week, but the graph shows the comparative speeds of the algorithms quite well. It also shows that the algorithm speed scales linearly with time allotted for executions which is what I was hoping for.

--- noise_algorithm_tests.py
+++ noise_algorithm_tests.py
@@ -3,48 +3,13 @@ import random
 import time
 from time import perf_counter_ns
 from noise import pnoise2, pnoise3, snoise2, snoise3
+import json
 
 def export(raw_tr):
-    unformated_tr = average(raw_tr)
-    test_results = format_tr(unformated_tr)
-
-    filename = "noise_results_" + time.strftime("%H.%M.%S_%d.%m.%Y", time.gmtime()) + ".txt"
+    filename = "noise_results_" + time.strftime("%Y.%m.%d_%H.%M.%S", time.gmtime()) + ".txt"
     f = open(filename, "w")
-    f.write(test_results)
+    f.write(json.dumps(raw_tr))
     f.close()
-
-def average(raw_tr):
-    tr_averages = []
-
-    # Parses data into num of executions per microsecond
-    for func_tests in raw_tr:
-        temp_average = [0, 0, 0, 0, 0] # Milli, deci, centi, second, total
-        
-        # Sums values in all test_batches for each speed and total average
-        for test_batch in func_tests:
-            temp_average[0] += test_batch[0] # Millisecond test
-            temp_average[1] += test_batch[1] # Decisecond test
-            temp_average[2] += test_batch[2] # Centisecond test
-            temp_average[3] += test_batch[3] # Second test
-            temp_average[4] += sum(test_batch) / 4
-
-        # Averages values in temp_average
-        for value in range(len(temp_average)):
-            temp_average[value] = temp_average[value] / len(func_tests)
-
-        tr_averages.append(temp_average) 
-
-    return tr_averages
-
-def format_tr(test_results):
-    output = "Order of averages: Milli, Deci, Centi, Second, Total"
-
-    output += "\nPerlin2D: \n" + ", ".join(str(x) for x in test_results[0])
-    output += "\nPerlin3D: \n" + ", ".join(str(x) for x in test_results[1])
-    output += "\nSimplex2D: \n" + ", ".join(str(x) for x in test_results[2])
-    output += "\nSimplex3D: \n" + ", ".join(str(x) for x in test_results[3])
-
-    return output
 
 
 # Random permuation arrays with 8193 elements each, used as inputs for noise algorithms, programatically generated
@@ -64,7 +29,7 @@ def test_speed(func, run_time):
         # The bitwise & operator prevents accessing a number past the array length
         func(X_COORDS[execs & 8192], Y_COORDS[execs & 8192], Z_COORDS[execs & 8192])
         execs += 1
-    return (execs / (perf_counter_ns() - time_start)) * 10**3 # Num of excecutions per microsecond
+    return [execs, perf_counter_ns() - time_start] # Num of excecutions per microsecond
 
 # Noise algorithms
 def perlin_2D(x, y, _z): pnoise2(x, y)
@@ -90,7 +55,7 @@ if __name__ == '__main__':
         for n in range(100):
             test = []
 
-            test.append(test_speed(f, 0.001)) # 1 Milisecond
+            test.append(test_speed(f, 0.001)) # 1 Millisecond
             test.append(test_speed(f, 0.01))  # 1 Centisecond
             test.append(test_speed(f, 0.1))   # 1 Decisecond
             test.append(test_speed(f, 1))     # 1 Second
@@ -99,3 +64,6 @@ if __name__ == '__main__':
         raw_test_results.append(test_batch)
 
     export(raw_test_results)
+
+# Make a log log plot of the data (x = time allocated for executions, y = number of executions)
+# Don't average data here, do it in the processing file
